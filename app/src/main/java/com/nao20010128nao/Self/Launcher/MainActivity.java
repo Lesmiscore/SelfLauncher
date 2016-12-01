3e8300c6f342a7c21b422d6ed22c0d9731054233
nao20010128nao <nao20010128@gmail.com>
2016-12-01 14:24:08 +0900

    読み込み画面

--- app/src/main/java/com/nao20010128nao/Self/Launcher/MainActivity.java
+++ app/src/main/java/com/nao20010128nao/Self/Launcher/MainActivity.java
@@ -1,36 +1,28 @@
 package com.nao20010128nao.Self.Launcher;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.SimpleExpandableListAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import android.content.*;
+import android.content.pm.*;
+import android.net.*;
+import android.os.*;
+import android.support.v4.view.*;
+import android.support.v7.app.*;
+import android.util.*;
+import android.view.*;
+import android.widget.*;
+import java.util.*;
+import android.text.*;
 
 public class MainActivity extends AppCompatActivity implements ExpandableListView.OnChildClickListener {
 	String action=Intent.ACTION_MAIN,category=Intent.CATEGORY_LAUNCHER,data=null;
 	final int editorResult=20;
 	ExpandableListView elv;
+	FrameLayout loading;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main3);
 		elv = (ExpandableListView)findViewById(R.id.lnList);
+		loading=(FrameLayout)findViewById(R.id.loading);
 		updateList();
 		elv.setOnChildClickListener(this);
     }
@@ -92,36 +84,50 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
 		}
     }
 	public void updateList() {
-		//elv.setAdapter((ExpandableListAdapter)null);
-		List<Map<String, String>> groupList = new ArrayList<Map<String,String>>();
-        List<List<Map<String, String>>> childList = new ArrayList<List<Map<String,String>>>();
-		for (ResolveInfo i:sort(getLauncheableActivityList(getPackageManager(), null), getPackageManager())) {
-			Map<String, String> groupElement = new HashMap<String, String>();
-			groupElement.put("GROUP_TITLE", "" + i.loadLabel(getPackageManager()));
-			groupList.add(groupElement);
-			List<Map<String, String>> childElements = new ArrayList<Map<String, String>>();
-			for (String j:new String[]{getResources().getString(R.string.openThis),
-				i.activityInfo.packageName,
-				i.activityInfo.name}) {
-				Map<String, String> child = new HashMap<String, String>();
-				child.put("CHILD_TITLE", j);
-				child.put("SUMMARY", "");
-				childElements.add(child);
+		elv.setVisibility(View.GONE);
+		loading.setVisibility(View.VISIBLE);
+		new AsyncTask<Void,Void,Map.Entry<List<Map<String, String>>,List<List<Map<String, String>>>>>(){
+			public Map.Entry<List<Map<String, String>>,List<List<Map<String, String>>>> doInBackground(Void... a){
+				List<Map<String, String>> groupList = new ArrayList<Map<String,String>>();
+				List<List<Map<String, String>>> childList = new ArrayList<List<Map<String,String>>>();
+				for (ResolveInfo i:sort(getLauncheableActivityList(getPackageManager(), null), getPackageManager())) {
+					Map<String, String> groupElement = new HashMap<String, String>();
+					groupElement.put("GROUP_TITLE", "" + i.loadLabel(getPackageManager()));
+					groupList.add(groupElement);
+					List<Map<String, String>> childElements = new ArrayList<Map<String, String>>();
+					for (String j:new String[]{getResources().getString(R.string.openThis),
+						i.activityInfo.packageName,
+						i.activityInfo.name}) {
+						Map<String, String> child = new HashMap<String, String>();
+						child.put("CHILD_TITLE", j);
+						child.put("SUMMARY", "");
+						childElements.add(child);
+					}
+					childList.add(childElements);
+				}
+				Map<List<Map<String, String>>,List<List<Map<String, String>>>> resultGen=new HashMap<>();
+				resultGen.put(groupList,childList);
+				return new ArrayList<Map.Entry<List<Map<String, String>>,List<List<Map<String, String>>>>>(resultGen.entrySet()).get(0);
 			}
-			childList.add(childElements);
-		}
-		SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
-			this.getApplicationContext(),
-			groupList,
-			R.layout.simple_expandable_list_item_1_custom,
-			new String []{"GROUP_TITLE"},
-			new int []{android.R.id.text1},
-			childList,
-			R.layout.simple_expandable_list_item_2_custom,
-			new String []{"CHILD_TITLE", "SUMMARY"},
-			new int []{android.R.id.text1, android.R.id.text2}
-		);
-		elv.setAdapter(adapter);
+			public void onPostExecute(Map.Entry<List<Map<String, String>>,List<List<Map<String, String>>>> d){
+				List<Map<String, String>> groupList = d.getKey();
+				List<List<Map<String, String>>> childList = d.getValue();
+				SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+					MainActivity.this,
+					groupList,
+					R.layout.simple_expandable_list_item_1_custom,
+					new String []{"GROUP_TITLE"},
+					new int []{android.R.id.text1},
+					childList,
+					R.layout.simple_expandable_list_item_2_custom,
+					new String []{"CHILD_TITLE", "SUMMARY"},
+					new int []{android.R.id.text1, android.R.id.text2}
+				);
+				elv.setAdapter(adapter);
+				loading.setVisibility(View.GONE);
+				elv.setVisibility(View.VISIBLE);
+			}
+		}.execute();
 	}
 	public void openEditor() {
 		Intent editor=new Intent();
@@ -140,11 +146,11 @@ public class MainActivity extends AppCompatActivity implements ExpandableListVie
 		switch (requestCode) {
 			case editorResult:
 				action = activityResult.getStringExtra("ACTION");
-				if (action.length() == 0)action = null;//action=Intent.ACTION_MAIN;
+				if (TextUtils.isEmpty(action))action = null;//action=Intent.ACTION_MAIN;
 				category = activityResult.getStringExtra("CATEGORY");
-				if (category.length() == 0)category = Intent.CATEGORY_LAUNCHER;
+				if (TextUtils.isEmpty(category))category = Intent.CATEGORY_LAUNCHER;
 				data = activityResult.getStringExtra("DATA");
-				if (data.length() == 0)data = null;
+				if (TextUtils.isEmpty(data))data = null;
 				Log.d("DEBUG", "action:" + action + "/category:" + category + "/data:" + data);
 				Log.d("DEBUG", "action:" + (action == null) + "/category:" + (category == null) + "/data:" + (data == null));
 				//Log.d("DEBUG","action:"+action.length()+"/category:"+category.length()+"/data:"+data.length());
